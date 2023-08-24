131c180cd677c7f47df1f1383b5091ccc11b3e64
programmer <info2016frei@gmail.com>
2023-08-24 09:09:18 +0200

    add test hor victory

--- src/test.py
+++ src/test.py
@@ -21,10 +21,32 @@ class TestConnectFourEnv(unittest.TestCase):
         self.assertTrue(self.env.check_vertical_victory(col=3, player=1))
     
     def test_vertical_victory_top(self):
+        self.env.state = self.env.state * 0
         self.env.state[0:4, 4] = 1
         self.assertTrue(self.env.check_vertical_victory(col=4, player=1))
+
+    def test_horizontal_victory_start(self):
+        self.env.state = self.env.state * 0
+        self.env.state[3, 0:4] = 1
+        self.assertTrue(self.env.check_horizontal_victory(row=3, player=1))
     
+    def test_horizontal_victory_middle(self):
+        self.env.state = self.env.state * 0
+        self.env.state[2, 1:5] = 1
+        self.assertTrue(self.env.check_horizontal_victory(row=2, player=1))
+    
+    def test_horizontal_victory_end(self):
+        self.env.state = self.env.state * 0
+        self.env.state[1, 3:7] = 1
+        # print(self.env.state)
+        self.assertTrue(self.env.check_horizontal_victory(row=1, player=1))
+    
+    def test_horizontal_victory_false(self):
+        self.env.state = self.env.state * 0
+        self.env.state[1, 2:7] = 1
+        self.env.state[1, 5] = 2
+        self.assertFalse(self.env.check_horizontal_victory(row=1, player=1))
+
+
 if __name__ == "__main__":
     unittest.main()
-
-i
